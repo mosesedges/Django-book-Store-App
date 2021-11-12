352a0307197250f33f1bccebc1cc40532cc01316
mosesedges <mosesedges@gmail.com>
2021-11-12 23:44:02 +0100

    allowhost added

--- Book_store_App/settings.py
+++ Book_store_App/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['floating-sierra-78274.herokuapp.com']
 
 
 # Application definition
